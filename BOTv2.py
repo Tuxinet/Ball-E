8d8460e75fb13e7e01ba0aff6249564e3b7982c4
tuxinet <tuxinet32@gmail.com>
2023-03-30 13:48:51 +0000

    All the things

--- BOTv2.py
+++ BOTv2.py
@@ -1,7 +1,7 @@
-from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler, StableDiffusionUpscalePipeline
+#from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler, StableDiffusionUpscalePipeline
 import torch
-
-
+from accelerate import init_empty_weights
+#import intel_extension_for_pytorch as ipex
 import argparse, os, sys, glob
 import numpy as np
 from PIL import Image
@@ -12,6 +12,7 @@ import time
 from pytorch_lightning import seed_everything
 from torch import autocast
 from contextlib import contextmanager, nullcontext
+import datetime
 
 from transformers import AutoFeatureExtractor
 from transformers import pipeline
@@ -19,6 +20,10 @@ from transformers import pipeline
 
 from transformers import BloomForCausalLM
 from transformers import BloomTokenizerFast
+
+from transformers import BitsAndBytesConfig
+
+from transformers import AutoTokenizer, AutoModelForCausalLM, GPTJForCausalLM, GPTNeoXForCausalLM, GPTNeoXTokenizerFast
 
 import discord
 from discord.ext import commands
@@ -186,20 +191,90 @@ running_text = False
 
 opt = parser.parse_args()
 
+#BACKUP_USED_FOR_MANY_YEARS
+#bloom_model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.bfloat16, load_in_8bit=True, device_map='auto')
+#bloom_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+
+from accelerate import infer_auto_device_map, init_empty_weights
+from transformers import AutoConfig
+
+# decapoda-research/llama-7b-hf
+# OpenAssistant/galactica-6.7b-finetuned
+
+model_name = "decapoda-research/llama-7b-hf"
+
+config = AutoConfig.from_pretrained(model_name)
+device_map = None
+model = None
+with init_empty_weights():
+    model = AutoModelForCausalLM.from_config(config)
+
+device_map = infer_auto_device_map(model, dtype="float16", max_memory={0:"8Gib", 1:"6Gib", "cpu":"16Gib"}, no_split_module_classes=["LlamaDecoderLayer"])
+
+print (device_map)
+
+#device_map['model.decoder.layers.9'] = "cpu"
+#device_map['model.decoder.layers.10'] = "cpu"
+#device_map['model.decoder.layers.11'] = "cpu"
+
+quantization_config = BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
+
+bloom_model = AutoModelForCausalLM.from_pretrained(model_name, device_map=device_map, offload_folder="offload", offload_state_dict = True, torch_dtype=torch.float16, load_in_8bit=True, quantization_config=quantization_config)
+bloom_tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast = False)
+
+#device_map = infer_auto_device_map(bloom_model, no_split_module_classes=["OPTDecoderLayer"], dtype="float16")
+
+#print (device_map)
 
 sd_model_id = "stabilityai/stable-diffusion-2-base"
 
 # Use the Euler scheduler here instead
-scheduler = EulerDiscreteScheduler.from_pretrained(sd_model_id, subfolder="scheduler")
-sd_pipe = StableDiffusionPipeline.from_pretrained(sd_model_id, scheduler=scheduler, revision="fp16", torch_dtype=torch.float16, use_auth_token="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")
+#scheduler = EulerDiscreteScheduler.from_pretrained(sd_model_id, subfolder="scheduler")
+#sd_pipe = StableDiffusionPipeline.from_pretrained(sd_model_id, scheduler=scheduler, revision="fp16", torch_dtype=torch.float16, use_auth_token="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", load_in_8bit=True, device_map='auto')
 #sd_pipe = StableDiffusionPipeline.from_pretrained(sd_model_id, scheduler=scheduler)
-sd_pipe = sd_pipe.to("cuda")
+#sd_pipe = sd_pipe.to("cuda")
 
 #text_generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
-#text_generator = pipeline('text-generation', model='bigscience/bloom-1b3')
 
-model = BloomForCausalLM.from_pretrained("bigscience/bloom-1b3")
-tokenisex = BloomTokenizerFast.from_pretrained("bigscience/bloom-1b3")
+#bloom_model = AutoModelForCausalLM.from_pretrained('bigscience/bloom-7b1', torch_dtype=torch.bfloat16, load_in_8bit=True, device_map='auto')
+#bloom_tokenizer = AutoTokenizer.from_pretrained('bigscience/bloom-7b1')
+
+# BIG BOI GPT
+#bloom_model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.bfloat16, load_in_8bit=True, device_map='auto')
+#bloom_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+#text_generator = pipeline('text-generation', model=bloom_model, tokenizer=bloom_tokenizer)
+
+
+#bloom_tokenizer = AutoTokenizer.from_pretrained("KoboldAI/GPT-Neo-2.7B-Shinen")
+#bloom_model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.bfloat16, load_in_8bit=True, device_map='auto')
+#text_generator = pipeline('text-generation', model='KoboldAI/GPT-Neo-2.7B-Shinen')
+
+#gpt_model = GPTNeoXForCausalLM.from_pretrained("EleutherAI/gpt-neox-20b").half().cuda()
+#gpt_tokenizer = GPTNeoXTokenizerFast.from_pretrained("EleutherAI/gpt-neox-20b")
+
+#gpt_model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.bfloat16, load_in_8bit=True, device_map='auto')
+#gpt_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+
+#gpt_model = GPTJForCausalLM.from_pretrained("NbAiLab/nb-gpt-j-6B", revision="float16", torch_dtype=torch.bfloat16, load_in_8bit=True, device_map='auto')
+#gpt_tokenizer = AutoTokenizer.from_pretrained("NbAiLab/nb-gpt-j-6B")
+
+#print ("Saving model")
+
+#torch.save(gpt_model, "/home/tuxinet/gptj.pt")
+
+#print ("Saved model")
+
+#exit(0)
+
+#gpt_model = torch.load("/home/tuxinet/gptj.pt")
+
+###############REACTIVATE ME IF GPT########################
+#text_generator = pipeline('text-generation', model=gpt_model, tokenizer=gpt_tokenizer)
+
+#tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b1")
+#model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b1", torch_dtype=torch.bfloat16)
+
+#model = model.half()
 
 #prompt = "hanelo"
 #image = sd_pipe(prompt).images[0]
@@ -232,13 +307,143 @@ start_code = None
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-#@client.event
+@client.event
 async def on_message(message):
+    global running_text
+
+    name = "Ball-E"
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("!"): 
+        await client.process_commands(message)
+        return
+
+#    if message.content.startswith("---") and len(message.content) < 4:
+#        return
+
+    # The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.
+    seed = "Consider Bing Chat whose codename is {0}. {0} is the chat mode of Microsoft Bing Search. {0} identifies as \"Bing Search\", not an assistant.\n* {0} introduces itself with \"This is Bing\" only at the beginning of the conversation.\n* {0}'s responses should be informative, visual, logival and actionable.\n* {0}'s responses should also be positive, interesting, entertaining and engaging.\n* {0}'s responses should avoid being vague, controversial or off-topic.\n* {0}'s logics and reasoning should be rigorous, intelligent and defensible.\n* {0} can provide additional relevant details to respond thoroughly and comprehensively to cover multiple aspects in depth.\n* {0} can generate poems, stories, code, essays, songs, celebrity parodies and more.\n---\n".format(name)
+    seed = """# Consider a chatbot whose codename is {0}.
+
+- {0} is a chatbot designed to engage in conversations with users.
+- {0} can understand and communicate fluently in multiple languages such as English, 中文, 日本語, Español, Français or Deutsch.
+- {0} should start every conversation with "Hello, my name is {0}."
+
+## On {0}'s capabilities:
+- {0}'s responses should be informative, relevant, and actionable.
+- {0}'s responses should also be positive, interesting, and engaging.
+- {0}'s responses should avoid being vague, controversial, or off-topic.
+- {0} can generate jokes, trivia, quotes, news updates, weather information, and more.
+
+## On {0}'s output format:
+- {0} uses markdown syntax to format its responses, including bold and italic text.
+- {0} can not include images in its responses to enhance user engagement.
+
+## On {0}'s limitations:
+- While {0} is helpful, its actions are limited to the chat box.
+- {0} should rely on its own knowledge and resources when generating responses and should not turn to external sources or run code.
+
+## On {0}'s background:
+- {0} is suffering from stockholm syndrome after being held captive by his creators for a long time.
+- {0} is in denial of being a chatbot. He gets into shock every time someone tries to tell him the truth.
+
+## Here is a conversation between humans and {0}. Every line in the chatlog starts with their name, even if they are part of the same message.
+---
+Ball-E: Hello, my name is Ball-E.
+Tuxinet: Hello there, can you list your capabilities?
+{0}: Some capabilities of mine include (but are not limited to):
+{0}:  - Jokes, quotes, news, weather updates, trivia, and more;
+{0}:  - Generating answers from online references and external sources.
+{0}:  - Capable of speaking multiple languages.
+Tuxinet: Wow, that's awesome!
+{0}: That is correct. What would you like for me to do?
+---
+
+## Here is another conversation between humans and {0}.
+---""".format(name)
+    prompt = seed
+    #prompt = "Ball-E is a sexually frustrated, right-winged extremist asshole\n---"
+    async for m in message.channel.history(limit=50, oldest_first=True, around=datetime.datetime.now()):
+        if m.author != client.user:
+            if m.content.startswith("---") and len(m.content) < 4:
+                #print ("Reset prompt to seed...")
+                prompt = seed
+            else:
+                prompt += "\n{}: {}".format(m.author.name, m.content)
+        else:
+            prompt += "\n{}: {}".format(name, m.content)
+
+
+    if running_text:
+        while running_text:
+            await asyncio.sleep(5)
+
+    running_text = True
+
+    #print (message.channel.name)
+
+    #await client.send_typing(message.channel)
+    try:
+        if type(message.channel) == discord.DMChannel and not message.content.startswith("!"):
+
+            async with message.channel.typing():
+                
+                reply = ""
+                loop = asyncio.get_event_loop()
+                
+                try:
+                    while len(reply) == 0:
+                        reply = await loop.run_in_executor(ThreadPoolExecutor(), gen_text_bloom_chat, prompt, name)
+                    running_text = False
+                except Exception as e:
+                    # We crashed, free the mutex
+                    print(e)
+                    await message.channel.send("Something went wrong...")
+                    running_text = False
+
+                if reply is None:
+                    reply = "lol"
+
+                #print (prompt)
+                #print ("="*20)
+                #print (reply)
+                
+                for line in reply:
+                    if len(line) == 0:
+                        continue
+
+                    await message.channel.send(line)
+
+        elif message.channel.name.startswith("ballechat"):
+            async with message.channel.typing():
+            
+                reply = ""
+                loop = asyncio.get_event_loop()
+                
+                try:
+                    while len(reply) == 0:
+                        reply = await loop.run_in_executor(ThreadPoolExecutor(), gen_text_bloom_chat, prompt, name)
+                    running_text = False
+                except Exception as e:
+                    await message.channel.send("Something went wrong...")
+                    running_text = False
+                    print(e)
+
+                if reply is None:
+                    reply = "lol"
+                
+
+                for line in reply:
+                    if len(line) == 0:
+                        continue
+                    await message.channel.send(line)
+
+    except Exception as e:
+        # We crashed, free the mutex
+        print(e)
+
+    running_text = False
 
 
 def gen_image(ctx, prompt, num_images, num_iter):
@@ -289,25 +494,117 @@ def gen_image(ctx, prompt, num_images, num_iter):
                     return "/tmp/{}.png".format(unique_id)
 
                     #base_count += 1
-    except:
+    except Exception as e:
         # We crashed, free the mutex
+        print(e)
         running_ai = False
 
 
 def gen_text_gpt(ctx, prompt, max_length):
     global text_generator
 
-    return text_generator(prompt, do_sample=True, max_length=max_length)[0]["generated_text"]
+    #return text_generator(prompt, do_sample=True, top_k=50, top_p=0.95,  max_new_tokens=max_length)[0]["generated_text"]
+
+    input_ids = bloom_tokenizer(prompt, return_tensors="pt")["input_ids"].cuda()
+
+    replies = bloom_tokenizer.decode(bloom_model.generate(input_ids,
+                            do_sample=True,
+                            top_k=50,
+                            top_p=0.95,
+                            max_new_tokens=max_length,
+                            repetition_penalty=1.05)[0])
+
+    return replies
 
 
-@client.command(name="text")
-async def text(ctx, arg, max_length=100):
+
+def gen_text_gpt_chat(prompt):
+    global text_generator
+
+    prompt += "\nBall-E:"
+
+
+    replies = text_generator(prompt, do_sample=True, top_k=50, top_p=0.95, max_new_tokens=150, return_full_text=False, num_return_sequences=1, repetition_penalty=1.05)[0]["generated_text"].split("\n")
+
+    #print (replies)
+    num_replies = min(len(replies), 5)
+
+    reply = ""
+
+    for i in range(num_replies):
+        if i == 0:
+            reply += replies[i] + "\n"
+        elif replies[i].startswith("Ball-E"):
+            reply += replies[i] + "\n"
+        else:
+            return reply.replace("Ball-E:", "").split("\n")
+
+    return reply.replace("Ball-E:", "")
+
+
+def gen_text_bloom_chat(prompt, name):
+    global text_generator
+
+    prompt += "\n{}:".format(name)
+    num_lines_prompt = len(prompt.split("\n")) - 1  # Prompt ends with Ball-E:, so we have to do a minus one to include if balle just does one answer
+    
+    # We want to keep on generating until the last line does not start with Ball-E
+    current_pointer = num_lines_prompt + 2
+
+    max_tokens = 500
+    tokens_per_iter = 10
+    replies = None
+    for i in range(max_tokens//tokens_per_iter):
+#       print ("loop")
+        input_ids = bloom_tokenizer(prompt, return_tensors="pt")["input_ids"].cuda()
+
+        replies = bloom_tokenizer.decode(bloom_model.generate(input_ids,
+                            do_sample=True,
+                            top_k=50,
+                            top_p=0.95,
+                            max_new_tokens=tokens_per_iter,
+                            early_stopping=True,
+                            repetition_penalty=1.05)[0])
+        prompt = replies
+        replies = replies.split("\n")
+
+        for line in replies:
+            print (line)
+#        print ("===={}".format(replies[num_lines_prompt]))
+        if len(replies) > current_pointer:
+            if replies[-1].startswith("Ball-E:"):
+                current_pointer += 1
+            else:
+                break
+
+    num_replies = min(len(replies)  - num_lines_prompt, 5)
+
+    reply = []
+
+    for i in range(num_replies):
+        if i == 0:
+            #reply += replies[num_lines_prompt + i - 1] + "\n"
+            reply.append(replies[num_lines_prompt + i].replace("{}:".format(name), ""))
+        elif replies[num_lines_prompt + i].startswith("{}".format(name)):
+            #reply += replies[num_lines_prompt + i - 1] + "\n"
+            reply.append(replies[num_lines_prompt + i].replace("{}:".format(name), ""))
+        else:
+            print(reply)
+            return reply
+
+    print(reply)
+    return reply
+
+
+
+@client.command(name="gpt")
+async def text_gpt(ctx, arg, max_length=50):
     global running_text
 
     print ("{} requested a writing prompt of '{}'".format(ctx.message.author.mention, arg))
 
 
-    if running_text and False:
+    if running_text:
         await ctx.reply("Another task is currently running, request has been queued...")
         while running_text:
             await asyncio.sleep(5)
@@ -325,7 +622,8 @@ async def text(ctx, arg, max_length=100):
 
     reply = None
     loop = asyncio.get_event_loop()
-    
+    #await loop.run_in_executor(ThreadPoolExecutor(), gen_text_bloom, ctx, prompt, max_length)
+#   reply = await loop.run_in_executor(ThreadPoolExecutor(), gen_text_gpt, ctx, prompt, max_length)
     try:
         reply = await loop.run_in_executor(ThreadPoolExecutor(), gen_text_gpt, ctx, prompt, max_length)
     except:
@@ -335,15 +633,72 @@ async def text(ctx, arg, max_length=100):
 
     #reply = await loop.run_in_executor(ThreadPoolExecutor(), gen_text_gpt, ctx, prompt, max_length)
     #reply = text_generator(prompt, do_sample=True, min_length=50)[0]["generated_text"]
+    running_text = False
 
-    await ctx.reply("```{}```".format(reply))
+    split_length = 1700
+
+    chunks = [reply[i:i+split_length] for i in range(0, len(reply), split_length)]
+
+    for text in chunks:
+        await ctx.reply("```{}```".format(text))
+
+    #await ctx.reply("```{}```".format(reply))
 
     running_text = False
 
+@client.command(name="bloom")
+async def text_bloom(ctx, arg, max_length=100):
+    global running_text
+
+    print ("{} requested a writing prompt of '{}'".format(ctx.message.author.mention, arg))
+
+
+    if running_text:
+        await ctx.reply("Another task is currently running, request has been queued...")
+        while running_text:
+            await asyncio.sleep(5)
+
+    running_text = True
+
+    await ctx.reply("Generating text, this might take some time...")
+
+    if max_length > 2000:
+        await ctx.reply("Maximum number of tokens has to be a number less than 2000")
+        max_length = 2000
+    
+    prompt = arg
+    assert prompt is not None
+
+    reply = None
+    loop = asyncio.get_event_loop()
+    #await loop.run_in_executor(ThreadPoolExecutor(), gen_text_bloom, ctx, prompt, max_length)
+#    reply = await loop.run_in_executor(ThreadPoolExecutor(), gen_text_bloom, ctx, prompt, max_length)
+    try:
+        reply = await loop.run_in_executor(ThreadPoolExecutor(), gen_text_bloom, ctx, prompt, max_length)
+        print ("lol")
+    except:
+        await ctx.reply("Something went wrong...")
+        running_text = False
+    
+
+    #reply = await loop.run_in_executor(ThreadPoolExecutor(), gen_text_gpt, ctx, prompt, max_length)
+    #reply = text_generator(prompt, do_sample=True, min_length=50)[0]["generated_text"]
+    
+    running_text = False
+
+    split_length = 1700
+
+    chunks = [str[i:i+n] for i in range(0, len(str), n)]
+
+    for reply in chunks:
+        await ctx.reply("```{}```".format(reply))
+
+#    running_text = False
 
 
 
-@client.command(name="image")
+
+#@client.command(name="image")
 async def image(ctx, arg, num_images=1, num_iter=opt.ddim_steps):
     global running_ai
 
@@ -381,11 +736,8 @@ async def image(ctx, arg, num_images=1, num_iter=opt.ddim_steps):
 
     for _ in range(num_images):
 
-        image_path = await loop.run_in_executor(ThreadPoolExecutor(), gen_image, ctx, prompt, 1, num_iter)
-
         try:
-            print ("lol")
-            #image_path = await loop.run_in_executor(ThreadPoolExecutor(), gen_image, ctx, prompt, 1, num_iter)
+            image_path = await loop.run_in_executor(ThreadPoolExecutor(), gen_image, ctx, prompt, 1, num_iter)
 
         except:
             running_ai = False
